b407dda75d460c472701c5874dcab88f8389d4e7
“adamg” <adam@queuedesk.org>
2026-04-22 22:32:05 +0100

    full sql

--- lbbk8.sql
+++ lbbk8.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2026 at 10:55 PM
+-- Generation Time: Apr 22, 2026 at 11:31 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -24,6 +24,86 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `blocked`
+--
+
+CREATE TABLE `blocked` (
+  `blockedID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dm`
+--
+
+CREATE TABLE `dm` (
+  `sender` varchar(255) NOT NULL,
+  `reciever` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `following`
+--
+
+CREATE TABLE `following` (
+  `followinguser_id` int(11) NOT NULL,
+  `followingname` varchar(255) NOT NULL,
+  `followuser_id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `followid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gc_ids`
+--
+
+CREATE TABLE `gc_ids` (
+  `chatid` int(11) NOT NULL,
+  `gcname` varchar(255) NOT NULL,
+  `members` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gc_members`
+--
+
+CREATE TABLE `gc_members` (
+  `id` int(11) NOT NULL,
+  `chatid` int(11) NOT NULL,
+  `gccount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gc_messages`
+--
+
+CREATE TABLE `gc_messages` (
+  `messageid` int(11) NOT NULL,
+  `chatid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `nsfw` bit(1) NOT NULL DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `messages`
 --
 
@@ -36,9 +116,80 @@ CREATE TABLE `messages` (
   `userid` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `username` varchar(255) DEFAULT NULL,
+  `pfp` varchar(255) DEFAULT NULL,
+  `bio` varchar(255) DEFAULT NULL,
+  `PROCOLOUR` varchar(255) DEFAULT '#1877f2',
+  `bimg` varchar(255) DEFAULT '../images/default.png'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reactions`
+--
+
+CREATE TABLE `reactions` (
+  `id` int(11) NOT NULL,
+  `messageid` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `reaction` enum('like','dislike','love','shock') DEFAULT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `preferred_mode` varchar(10) DEFAULT 'light',
+  `following` varchar(255) DEFAULT '',
+  `kids` varchar(10) NOT NULL DEFAULT 'off',
+  `apikey` varchar(64) DEFAULT 'NOT CREATED',
+  `verified` bit(1) DEFAULT b'0',
+  `saved` varchar(255) DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `following`
+--
+ALTER TABLE `following`
+  ADD PRIMARY KEY (`followid`);
+
+--
+-- Indexes for table `gc_ids`
+--
+ALTER TABLE `gc_ids`
+  ADD PRIMARY KEY (`chatid`);
+
+--
+-- Indexes for table `gc_members`
+--
+ALTER TABLE `gc_members`
+  ADD PRIMARY KEY (`gccount`);
+
+--
+-- Indexes for table `gc_messages`
+--
+ALTER TABLE `gc_messages`
+  ADD PRIMARY KEY (`messageid`),
+  ADD KEY `chatid` (`chatid`);
 
 --
 -- Indexes for table `messages`
@@ -47,13 +198,68 @@ ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD UNIQUE KEY `unique_username` (`username`);
+
+--
+-- Indexes for table `reactions`
+--
+ALTER TABLE `reactions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `message_id` (`messageid`,`username`,`reaction`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `following`
+--
+ALTER TABLE `following`
+  MODIFY `followid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gc_ids`
+--
+ALTER TABLE `gc_ids`
+  MODIFY `chatid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gc_members`
+--
+ALTER TABLE `gc_members`
+  MODIFY `gccount` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gc_messages`
+--
+ALTER TABLE `gc_messages`
+  MODIFY `messageid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reactions`
+--
+ALTER TABLE `reactions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
